72b9da6e7c51bef53824c490df0a0ca07f130015
kiyo3588 <kiyoohara0627@gmail.com>
2023-01-03 08:40:31 +0000

    6. 2. 3 エラーメッセージを表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:passeword])
     else
+      flash.now[:danger] = '認証に失敗しました。'
     render :new
+    end
   end
-  
 end
