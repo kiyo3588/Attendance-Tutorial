672041e1f72e31065851e673a7ff906a119ff4e8
kiyo3588 <kiyoohara0627@gmail.com>
2023-01-04 04:26:49 +0000

    6. 4 レイアウトのログイン＆ログアウト仕様対応

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,14 @@ module SessionsHelper
   end
   
    # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
-   def current_user
-     if session[:user_id]
+  def current_user
+    if session[:user_id]
        @current_user ||= User.find_by(id: session[:user_id])
-     end
-   end
+    end
+  end
+   
+    # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
 end
