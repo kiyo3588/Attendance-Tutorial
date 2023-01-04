5268909967067256fc2fcbf73f50613eeb67018b
kiyo3588 <kiyoohara0627@gmail.com>
2023-01-04 04:00:32 +0000

    6. 3. 2 current_userを定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:passeword])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
     render :new
