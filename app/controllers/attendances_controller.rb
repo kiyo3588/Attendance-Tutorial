0d8d497be43fbd9fab850399198fd0792977e6f3
kiyo3588 <kiyoohara0627@gmail.com>
2023-02-02 23:57:39 +0000

    11. 2 StrongParametersの応用

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
   
@@ -15,20 +15,25 @@ class AttendancesController < ApplicationController
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end
-   elsif @attendance.finished_at.nil?
-     if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
-       flash[:info] = "お疲れ様でした。"
-     else
-       flash[:danger] = UPDATE_ERROR_MSG
-     end
-   end
-     redirect_to @user
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    end
+    redirect_to @user
   end
   
   def edit_one_month
   end
   
   def update_one_month
-    debugger
   end
+  
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[attendances]
+    end
 end
